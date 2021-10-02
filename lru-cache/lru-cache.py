5da578d9e2acdf188e4f09c16681c258f057969c
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-10-02 19:53:16 +0530

    Time: 1030 ms (33.09%), Space: 76.2 MB (31.30%) - LeetHub

--- lru-cache/lru-cache.py
+++ lru-cache/lru-cache.py
@@ -1,25 +1,50 @@
+class Node:
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.prev = None
+        self.next =  None
+
+
 class LRUCache:
 
     def __init__(self, capacity):
-        self.has = OrderedDict()
+        self.has = {}
         self.cap = capacity
+        self.root = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.tail.prev = self.root
+        self.root.next = self.tail
         
     def get(self, key):
-        
         if key in self.has:
-            val = self.has[key]
-            self.has.pop(key)
-            self.has[key] = val
+            val = self.has[key].data
+            self.remove(self.has[key])
+            self.has[key] = Node(key, val)
+            self.add(self.has[key])
             return val
         return -1
     
     def put(self, key, value):
         if key in self.has:
-            self.has.pop(key)
-            self.has[key] = value
+            self.remove(self.has[key])
+            self.has[key] = Node(key, value)
+            self.add(self.has[key])
         else:
-            self.has[key] = value    
+            self.has[key] = Node(key, value)
+            self.add(self.has[key])
         if len(self.has) == self.cap+1:
-            for i in self.has:
-                self.has.pop(i)
-                break
+            self.has.pop(self.root.next.key)
+            self.remove(self.root.next)
+            
+    def add(self, node):
+        c = self.tail.prev
+        c.next = node
+        node.prev = c
+        node.next = self.tail
+        self.tail.prev = node
+        
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        
