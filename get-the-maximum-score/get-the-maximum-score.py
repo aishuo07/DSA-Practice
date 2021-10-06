7cfe124cd73ac8aa65eaeca8e648c9a8e1034337
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-10-06 12:51:58 +0530

    Time: 1044 ms (14.28%), Space: 140.6 MB (14.29%) - LeetHub

--- get-the-maximum-score/get-the-maximum-score.py
+++ get-the-maximum-score/get-the-maximum-score.py
@@ -3,20 +3,26 @@ class Solution:
         dic = {i:j for j, i in enumerate(nums1)}
         dic1 = {i:j for j, i in enumerate(nums2)}
         mod = 10**9 + 7
-        @cache
+        dp = [[-1, -1] for _ in range(max(len(nums1), len(nums2)))] 
         def rec(i, flag):
             if flag == 0:
                 if i >= len(nums1):
                     return 0
+                elif dp[i][flag] !=-1:
+                    return dp[i][flag]
                 elif nums1[i] in dic1:
-                    return nums1[i] + max(rec(i+1, flag), rec(dic1[nums1[i]]+1, 1))
+                    ans = nums1[i] + max(rec(i+1, flag), rec(dic1[nums1[i]]+1, 1))
                 else:
-                    return nums1[i] + rec(i+1, flag)
+                    ans = nums1[i] + rec(i+1, flag)
             else:
                 if i >= len(nums2):
                     return 0
+                elif dp[i][flag] !=-1:
+                    return dp[i][flag]
                 elif nums2[i] in dic:
-                    return nums2[i] + max(rec(i+1, flag), rec(dic[nums2[i]]+1, 0))
+                    ans = nums2[i] + max(rec(i+1, flag), rec(dic[nums2[i]]+1, 0))
                 else:
-                    return nums2[i] + rec(i+1, flag)
+                    ans = nums2[i] + rec(i+1, flag)
+            dp[i][flag] = ans
+            return ans
         return max(rec(0, 0), rec(0, 1))%mod
