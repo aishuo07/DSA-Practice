9936e835e0053d9c5e4d01ae0a1c5f03a34e9915
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-10-12 02:50:03 +0530

    Time: 1542 ms (9.83%), Space: 78.7 MB (8.85%) - LeetHub

--- lfu-cache/lfu-cache.py
+++ lfu-cache/lfu-cache.py
@@ -14,12 +14,12 @@ class LFUCache:
             self.count[count][key] = None
             
     def remove(self, key):
-        c, d = self.dic.pop(key)
-        self.count[c].pop(key)
+        self.dic.pop(key)
     
     def get(self, key):
         if key in self.dic:
             c, d = self.dic[key]
+            self.count[c].pop(key)
             self.remove(key)
             self.add(key, d, c + 1)
             return self.dic[key][1]
@@ -30,15 +30,14 @@ class LFUCache:
             return
         if key in self.dic:
             c, d = self.dic[key]
+            self.count[c].pop(key)
             self.remove(key)
             self.add(key, value, c + 1)
         else:
             if len(self.dic)==self.cap:
                 while not self.count[self.ind]:
                     self.ind += 1
-                for i in self.count[self.ind]:
-                    c = i
-                    break
+                c, d = self.count[self.ind].popitem(last = False)
                 self.remove(c)
                 self.ind = 1
             self.add(key, value, 1)
