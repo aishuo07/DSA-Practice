48b7df199f4b7ddd741578d60f24aa9d4383b417
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-10-25 01:34:32 +0530

    Time: 57 ms (30.93%), Space: 14.6 MB (25.24%) - LeetHub

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -1,15 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordDict = set(wordDict)
         n = len(s)
         @cache
         def rec(i):
-            if i == n:
+            if i >= n:
                 return True
             t = ''
+            ans = False
             for j in range(i, n):
-                t+=s[j]
-                if t in wordDict and rec(j+1):
-                    return True
-            return False
+                t += s[j]
+                if t in wordDict:
+                    ans |= rec(j+1)
+            return ans
         return rec(0)
-                    
