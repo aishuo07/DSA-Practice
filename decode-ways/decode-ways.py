6a9e938d61de83d25d002e97afcf637b74632116
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-10-24 14:40:39 +0530

    Time: 42 ms (37.06%), Space: 14.5 MB (16.06%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -1,17 +1,15 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        n = len(s)
         @cache
         def rec(i):
-            if i == len(s):
+            if i >= len(s):
                 return 1
-            if i> len(s):
-                return 0
-            a = 0
+            ans = 0
             if s[i] == '0':
                 return 0
-            if 0<int(s[i])<27:
-                a += rec(i+1)
-            if 0<int(s[i:i+2])<27:
-                a+=rec(i+2)
-            return a
+            for j in range(i+1, min(len(s)+1, i+3)):
+                if s[i]!=0 and 0<int(s[i:j])<27:
+                    ans+= rec(j)
+            return ans
         return rec(0)
