8cd963a4279cfae2508a4fe9447077622723f21b
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-10-25 09:25:33 +0530

    Time: 2432 ms (75.03%), Space: 44.8 MB (6.41%) - LeetHub

--- concatenated-words/concatenated-words.py
+++ concatenated-words/concatenated-words.py
@@ -1,18 +1,40 @@
+class trie:
+    
+    def __init__(self):
+        self.child = defaultdict(trie)
+        self.end = False
+
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
-        d = set()
-        res = []
         words.sort(key = lambda x:len(x))
+        root = trie()
+        ans = []
+
+        def search(i):
+            temp = root
+            if not i:
+                return True
+            ans = False
+            if i in dp:
+                return dp[i]
+            for k, j in enumerate(i):
+                if j in temp.child:
+                    temp = temp.child[j]
+                    if temp.end:
+                        ans |= search(i[k+1:])
+                else:
+                    dp[i] = ans
+                    return ans
+            dp[i]= ans
+            return ans
+        
         for i in words:
-            dp = [0]*len(i) + [1]
-            for j in range(-1, len(i)):
-                s = ''
-                if dp[j]:
-                    for k in range(j+1, len(i)):
-                        s += i[k]
-                        if s in d:
-                            dp[k] = 1
-            if len(i)>=2 and dp[-2]:
-                res.append(i)
-            d.add(i)
-        return res
+            temp = root
+            dp = {}
+            if search(i) and i!='':
+                ans.append(i)
+            for j in i:
+                temp = temp.child[j]
+            temp.end = True
+            
+        return ans
