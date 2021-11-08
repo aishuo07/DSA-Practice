d97cd4bf53f36eb2362eb80b647ad14174fde4df
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-11-08 16:42:41 +0530

    Time: 28 ms (87.80%), Space: 14.3 MB (66.68%) - LeetHub

--- word-break-ii/word-break-ii.py
+++ word-break-ii/word-break-ii.py
@@ -1,15 +1,12 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        wordDict = set(wordDict)
         ans = []
-        n = len(s)
-        def rec(i, path):
-            if i == n:
-                ans.append(path[:-1])
-            t = ''
-            for j in range(i, n):
-                t += s[j]
-                if t in wordDict:
-                    rec(j+1, path + t + ' ')
+        def rec(i, temp):
+            if i == len(s):
+                ans.append(temp.rstrip(' '))
+            for j in range(i, len(s)):
+                if s[i:j+1] in wordDict:
+                    rec(j+1, temp + s[i:j+1] + ' ')
+            
         rec(0, '')
         return ans
