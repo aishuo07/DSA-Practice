cc75f9999bd7d30ce8085fae79d2bdf783a0944f
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-11-10 00:59:59 +0530

    Time: 32 ms (64.03%), Space: 14.4 MB (43.06%) - LeetHub

---
+++ implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -0,0 +1,24 @@
+class MyQueue:
+
+    def __init__(self):
+        self.input = []
+        self.output = []
+        
+
+    def push(self, x: int) -> None:
+        self.input.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.output.pop()
+        
+    def peek(self):
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+
+    def empty(self) -> bool:
+        if self.output or self.input:
+            return False
+        return True
