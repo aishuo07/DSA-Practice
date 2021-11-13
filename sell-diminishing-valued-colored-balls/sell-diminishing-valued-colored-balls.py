ba84217a20eb59224a68d0bca55d7ae7a77c51ee
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-11-13 10:39:53 +0530

    Time: 772 ms (53.10%), Space: 25.1 MB (59.79%) - LeetHub

--- sell-diminishing-valued-colored-balls/sell-diminishing-valued-colored-balls.py
+++ sell-diminishing-valued-colored-balls/sell-diminishing-valued-colored-balls.py
@@ -1,41 +1,19 @@
 class Solution:
-    def maxProfit(self, inventory: List[int], orders: int) -> int:
-        d = Counter(inventory)
-        h = []
-        ans = 0
-        mod = 10**9 + 7
+    def maxProfit(self, inventory, orders):
         def calc(n):
             return (n*(n+1))//2
-        for i in d:
-            heappush(h, [-i, d[i]])
-        while h and orders>0:
-            if len(h)>=2:
-                c = heappop(h)
-                d = heappop(h)
-                if c[0] ==d[0]:
-                    heappush(h, [c[0], c[1]+d[1]])
-                else:
-                    c[0] = abs(c[0])
-                    d[0] = abs(d[0])
-                    e = orders//c[1]
-                    orders -= min(e, (c[0]-d[0]))*c[1]
-                    ans = ans+(calc(c[0])-calc(c[0]-min(e, (c[0]-d[0]))))*c[1]
-                    c[0] -= min(e, (c[0]-d[0]))
-                    while orders>0 and c[0]>d[0]:
-                        ans += min(orders, c[1])*c[0]
-                        orders-=min(orders, c[1])
-                        c[0]-=1
-                    heappush(h, [-d[0], c[1]+d[1]])
+        inventory.sort(reverse = True)
+        i = 1
+        ans= 0
+        while orders>0:
+            if i<len(inventory) and orders>(i)*(inventory[i-1]-inventory[i]):
+                ans += (i)*(calc(inventory[i-1]) - calc(inventory[i]))
+                orders -= (i)*(inventory[i-1] - inventory[i])
             else:
-                c = heappop(h)
-                c[0] = abs(c[0])
-                d = orders//c[1]
-                orders -= d*c[1]
-                ans = (ans+ (calc(c[0]) - calc(c[0]-d))*c[1])%mod
-                c[0] -= d
-                while orders>0 and c[0]>0:
-                    ans = (ans + min(orders, c[1])*c[0])%mod
-                    orders-=min(orders, c[1])
-                    c[0]-=1
-        return ans%mod
-                
+                times = orders//(i)
+                orders = orders%i
+                ans += i*(calc(inventory[i-1]) - calc(inventory[i-1]-times))
+                ans += orders*(inventory[i-1]-times)
+                orders = 0
+            i+=1
+        return ans%1000000007
