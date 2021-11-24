6aa1c542a69741e1e9de242bd6f0be8dde1bc8d4
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-11-24 16:51:44 +0530

    Time: 718 ms (98.13%), Space: 103.4 MB (23.93%) - LeetHub

---
+++ count-primes/count-primes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countPrimes(self, n: int):
+        if n < 2:
+            return 0
+        strikes = [1] * n
+        strikes[0] = 0
+        strikes[1] = 0
+        for i in range(2, int(n**0.5)+1):
+            if  strikes[i] != 0:
+                strikes[i*i:n:i] = [0] * ((n-1-i*i)//i + 1)
+
+        return sum(strikes)
