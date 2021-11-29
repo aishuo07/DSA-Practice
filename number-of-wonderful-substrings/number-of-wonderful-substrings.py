b358c046ca9dab795025289cf1edb22ea1ad5edb
aishuo07 <45160006+aishuo07@users.noreply.github.com>
2021-11-29 15:36:53 +0530

    Time: 7220 ms (5.33%), Space: 15.2 MB (15.97%) - LeetHub

--- number-of-wonderful-substrings/number-of-wonderful-substrings.py
+++ number-of-wonderful-substrings/number-of-wonderful-substrings.py
@@ -1,14 +1,20 @@
 class Solution:
     def wonderfulSubstrings(self, word: str) -> int:
-        dp = defaultdict(int)
-        dp[0] = 1
-        mask = 0
-        ans = 0
-        for j in word:
-            
-            mask ^= 1<<(ord(j)-ord('a'))
-            ans += dp[mask]
-            for i in range(10):
-                ans += dp[mask ^ 1<<i]
-            dp[mask] += 1
-        return ans
+        dic = defaultdict(int)
+        s = ['0']*10
+        res = 0
+        dic['0000000000'] = 1
+        for i in word:
+            s[ord(i)-ord('a')] = str((int(s[ord(i)-ord('a')]) + 1)%2)
+            t = ''.join(s)
+            for j in range(10):
+                temp = s[j]
+                if s[j] == '1':
+                    s[j] = '0'
+                elif s[j] == '0':
+                    s[j] = '1'
+                res += dic[''.join(s)]
+                s[j] = temp
+            res += dic[t]
+            dic[t] += 1
+        return res
